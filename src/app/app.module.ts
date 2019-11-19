ed595fb05df0544fd8b7424fab856d39b1627ae8
Zaime Ali <zaimeali1997@gmail.com>
2019-11-19 22:01:50 +0500

    Import all Modules in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Updating the App Module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+// Mat => Material
+import 'hammerjs';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
