a7d93531195494b3125e2bcb80f67c21752a2f9a
Zaime Ali <zaimeali1997@gmail.com>
2019-12-01 21:55:00 +0500

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,53 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
-
-const DISHES: Dish[] = [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint: disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappizza'
-  },
-  {
-    id: '1',
-    name: 'Elachi Cheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '1.99',
-    // tslint: disable-next-line:max-line-length
-    description: 'A unique combination of Indian Elachi Cheese Cake'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '2.99',
-    // tslint: disable-next-line:max-line-length
-    description: 'A unique combination of Indian Vadonut'
-  },
-  {
-    id: '3',
-    name: 'Zucchi Pakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '3.99',
-    // tslint: disable-next-line:max-line-length
-    description: 'A unique combination of Indian Zucchi Pakoda'
-  }
-];
+import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
@@ -60,11 +14,15 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish[] = DISHES;
 
-  selectedDish: Dish = DISHES[0];
+  selectedDish: Dish;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(dish: Dish){
+    this.selectedDish = dish;
   }
 
 }
